f5ba775afa2b875936fba4e6b561376d8a0a1fbb
George Halal <georgech@stanford.edu>
2021-05-17 09:16:28 -0700

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -11,8 +11,10 @@ import torch
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_dir', default='data/original', help="Directory with the original dataset")
-parser.add_argument('--output_dir', default='data/preprocessed_binary', help="Where to write the new dataset")
+parser.add_argument('--input_dir', default='data/original', \
+                    help="Directory with the original dataset")
+parser.add_argument('--output_dir', default='data/preprocessed_binary', \
+                    help="Where to write the new dataset")
 
 
 def cut(df):
@@ -38,13 +40,20 @@ def modify_columns_and_concat(df_detect, df_undetect, skymaps, skymapnames):
         skymapnames: names of the maps containing the observing conditions
     """
 
-    df_undetect['pixel'] = hp.ang2pix(nside=4096, theta=df_undetect['true_dec'].apply(lambda dec: np.deg2rad(90 - dec)), phi = df_undetect['true_ra'].apply(lambda ra: np.deg2rad(ra)))
+    df_undetect['pixel'] = hp.ang2pix(nside=4096, theta=df_undetect['true_dec'].apply(lambda dec: \
+                                      np.deg2rad(90 - dec)), \
+                                      phi = df_undetect['true_ra'].apply(lambda ra: np.deg2rad(ra)))
     df_undetect = df_undetect[['pixel','detected','true_g','true_r','true_i','true_z']]
-    df_undetect[['true_g','true_r','true_i','true_z']] = df_undetect[['true_g','true_r','true_i','true_z']].apply(lambda m: 10**((30-m)/2.5))
+    df_undetect[['true_g','true_r','true_i','true_z']] = df_undetect[['true_g','true_r','true_i',\
+                                                                      'true_z']].apply(lambda m: \
+                                                                       10**((30-m)/2.5))
 
-    df_detect['pixel'] = hp.ang2pix(nside=4096, theta=df_detect['unsheared/dec'].apply(lambda dec: np.deg2rad(90 - dec)), phi = df_detect['unsheared/ra'].apply(lambda ra: np.deg2rad(ra)))
+    df_detect['pixel'] = hp.ang2pix(nside=4096, theta=df_detect['unsheared/dec'].apply(lambda dec: \
+                                    np.deg2rad(90 - dec)), \
+                                    phi = df_detect['unsheared/ra'].apply(lambda ra: np.deg2rad(ra)))
     df_detect['detected'] = np.ones(len(df_detect))
-    df_detect[['true_g','true_r','true_i','true_z']] = df_detect[['BDF_FLUX_DERED_G','BDF_FLUX_DERED_R','BDF_FLUX_DERED_I','BDF_FLUX_DERED_Z']]#.apply(lambda x: -2.5*np.log10(x)+30)
+    df_detect[['true_g','true_r','true_i','true_z']] = df_detect[['BDF_FLUX_DERED_G','BDF_FLUX_DERED_R',\
+                                                                  'BDF_FLUX_DERED_I','BDF_FLUX_DERED_Z']]
     df_detect = df_detect[['pixel','detected','true_g','true_r','true_i','true_z']]
 
     df = pd.concat([df_undetect, df_detect], ignore_index=True)
@@ -84,10 +93,14 @@ def split_and_normalize(df, output_dir):
     test_df, train_df, val_df = split(df)
     train_mean = np.array(train_df.mean()[2:])
     train_std = np.array(train_df.std()[2:])
-    train_df[[str(col) for col in train_df.columns][2:]] = (train_df[[str(col) for col in train_df.columns][2:]] - train_mean) / train_std
-    test_df[[str(col) for col in test_df.columns][2:]] = (test_df[[str(col) for col in test_df.columns][2:]] - train_mean) / train_std
-    val_df[[str(col) for col in val_df.columns][2:]] = (val_df[[str(col) for col in val_df.columns][2:]] - train_mean) / train_std
-    pd.DataFrame({'train_mean': train_mean, 'train_std': train_std}, index=[str(col) for col in train_df.columns][2:]).to_pickle(os.path.join(output_dir,'normalization.pkl'))
+    train_df[[str(col) for col in train_df.columns][2:]] = (train_df[[str(col) for col in \
+                                                            train_df.columns][2:]] - train_mean) / train_std
+    test_df[[str(col) for col in test_df.columns][2:]] = (test_df[[str(col) for col in \
+                                                          test_df.columns][2:]] - train_mean) / train_std
+    val_df[[str(col) for col in val_df.columns][2:]] = (val_df[[str(col) for col in \
+                                                        val_df.columns][2:]] - train_mean) / train_std
+    pd.DataFrame({'train_mean': train_mean, 'train_std': train_std}, index=[str(col) for col in \
+                    train_df.columns][2:]).to_pickle(os.path.join(output_dir,'normalization.pkl'))
     return {'test': test_df, 'train': train_df, 'val': val_df}
 
 
@@ -129,7 +142,8 @@ def preprocess(args):
     print("Saving preprocessed dataset...")
     for dslice in ['test', 'train', 'val']:
         saveloc = os.path.join(args.output_dir,dslice)
-        true = torch.from_numpy(split_df[dslice][['true_g','true_r','true_i','true_z']].values).float().contiguous()
+        true = torch.from_numpy(split_df[dslice][['true_g','true_r','true_i',\
+                                                  'true_z']].values).float().contiguous()
         cond = torch.from_numpy(split_df[dslice][skymapnames].values).float().contiguous()
         out = torch.from_numpy(split_df[dslice]['detected'].values).float().contiguous()
         torch.save(true, os.path.join(saveloc,'true.pth'))
